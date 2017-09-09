0630bc0262f2391d4fe7d227fb11ea330c889bf1
Karl <zhaoma@foxmail.com>
2017-09-09 18:14:08 +0800

    ..

--- Libraries/Data/Clients/DataHelper.cs
+++ Libraries/Data/Clients/DataHelper.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Kalendar.Zero.DB.Entity.Base;
+using Kalendar.Zero.Utility.Common;
 
 namespace Kalendar.Zero.Data.Clients
 {
     public class DataHelper
+        : BaseHelper, IBaseHelper
     {
+        private ApiTerminal.Entities.Channel ProxyChannel
+        {
+            get
+            {
+                var channel=new ApiTerminal.Entities.Channel();
+                channel.FillFrom(Channel);
+                return channel;
+            }
+        }
+
+        private ApiTerminal.Entities.Avatar ProxyAvatar
+        {
+            get
+            {
+                var avatar = new ApiTerminal.Entities.Avatar();
+                avatar.FillFrom(Avatar);
+                return avatar;
+            }
+        }
+
         public string Signin()
         {
-            return "";
+            var request = new ApiTerminal.Clients.Request.WoowRequest
+            {
+                ChannelSymbol = Channel.ChannelSymbol,
+                Method = "Signin",
+                Channel = ProxyChannel,
+                Avatar= ProxyAvatar,
+
+            };
+
+            return ReadFromProxy(request).JsonToObj<string>();
         }
 
         public AccountAvatarsPO ExchangeToken(string code)
@@ -57,6 +88,12 @@ namespace Kalendar.Zero.Data.Clients
         public SchedulePO UpdateSchedules(SchedulePO schedule)
         {
             return new SchedulePO();
+        }
+
+        private string ReadFromProxy(ApiTerminal.Clients.Request.WoowRequest request)
+        {
+            var r = new Utility.Common.BrowserClient();
+            return r.SendHttpRequest(Config.ProxyApiUri, false, "POST", request.ObjToJson(), null, null, null, "UTF-8", "application/json", "application/json");
         }
     }
 }
